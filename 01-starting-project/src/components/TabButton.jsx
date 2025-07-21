124cca3957669bb03b1fdfe604875ac624fe2bfe
Kauan <kauan.manzatoo@gmail.com>
2025-07-21 19:59:04 -0300

    53 - How NOT to Update the UI

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,10 @@
 export default function TabButton({ children, onSelect }) {
+  console.log("TAB BUTTON RENDERING");
   return (
     <li>
       <button onClick={onSelect} className="tab-button">
         {children}
       </button>
     </li>
-  )
-} 
+  );
+}
